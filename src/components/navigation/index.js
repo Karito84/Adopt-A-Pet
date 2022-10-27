2becec37d6579debff6310c75bcae90f32cc0bf9
KAROLINA_MUELLER <karolina.gawlowska@gmail.com>
2022-10-27 14:45:43 -0400

    change a tags with NavLinks and path to relative

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getPetTypes } from '../../api/petfinder';
 import Logo from '../../assets/logo.svg';
 import Search from '../search';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   const [petTypes, setPetTypes] = useState([]);
@@ -23,25 +24,26 @@ const Navigation = () => {
       </div>
       <ul className="nav-links">
         <li key="all">
-          {/* This link should have an activeClassName and exact prop */}
-          <a
-            href="/"
+          {/* This link should have an activeClassName and exact prop in v5, BUT in v6 by default, an active class is added to a <NavLink> component when it is active. */}
+          <NavLink
+            end to="/" 
             className="nav-link"
           >
+             {/* Have to add "end" to the NavLink instead of exact in v5, that way it will ensure this component isn't matched as "active" when its descendant paths are matched.*/}
             All Pets
-          </a>
+          </NavLink>
         </li>
         {petTypes
           ? petTypes.map((type) => (
               <li key={type.name}>
                 {/* These links should have an activeClassName prop */}
-                <a
-                  href={`/${type._links.self.href.split('/').pop()}`}
+                <NavLink
+                  to={type.name}
                   key={type.name}
                   className="nav-link"
                 >
                   {type.name}s
-                </a>{' '}
+                </NavLink>{' '}
               </li>
             ))
           : 'Loading...'}
